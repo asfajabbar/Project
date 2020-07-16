9bae3729beb5f70a232ac9e93e421e50632cac9f
asfajabbar <60046127+asfajabbar@users.noreply.github.com>
2020-07-17 00:25:27 +0500

    Add files via upload

---
+++ Video.java
@@ -0,0 +1,30 @@
+package com.example.project;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import androidx.support.v7.app.AppCompatActivity;
+
+
+public class Video extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+    }
+}
